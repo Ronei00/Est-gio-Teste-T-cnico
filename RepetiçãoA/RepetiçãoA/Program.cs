5a13c9530121ba99995e2eebdf70ac90b2091184
Ronei <128158454+Ronei00@users.noreply.github.com>
2024-08-30 15:35:14 -0300

    Add files via upload

---
+++ RepetiçãoA/RepetiçãoA/Program.cs
@@ -0,0 +1,15 @@
+﻿int count = 0;
+Console.WriteLine("Escreva alguma frase e iremos contar quantos a tem na frase");
+
+string valordigitado = Console.ReadLine();
+
+char[] valor = valordigitado.ToCharArray();
+
+for(int i = 0; i < valor.Length; i++)
+{
+    if (valor[i] == 'a' || valor[i] == 'A')
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Nesta frase existe: {count} a");
